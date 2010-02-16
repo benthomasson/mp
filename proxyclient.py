a77b2f7df20a600a4df6a4641e5afc0f0dcd8f87
Ben Thomasson <bthomass@cisco.com>
2010-02-15 21:47:53 -0500

    Adding debugs and getting Thingy only once

--- proxyclient.py
+++ proxyclient.py
@@ -6,6 +6,7 @@ from proxies import ThingyProxy
 import time
 import gc
 from IPython.Shell import IPShellEmbed
+import traceback
 
 ipshell = IPShellEmbed()
 
@@ -17,15 +18,24 @@ ThingyManager.register('Thingy',proxytype=ThingyProxy)
 
 while True:
     try:
+        print 'Creating local manager'
         manager = ThingyManager(address=('127.0.0.1',8001), authkey='abracadabra')
+        print 'Connecting manager'
         manager.connect()
+        print 'Getting Thingy'
+        thingy = manager.Thingy()
         while True:
-            thingy = manager.Thingy()
             print thingy.name
             time.sleep(1)
+            print '.'
     except Exception, e:
         print str(e), e.__class__
+        print traceback.format_exc()
+    print 'Forgetting manager'
     manager = None
+    print 'Forgetting thingy'
     thingy = None
+    print 'Collecting garbage'
     gc.collect()
+    print 'Sleeping'
     time.sleep(1)
